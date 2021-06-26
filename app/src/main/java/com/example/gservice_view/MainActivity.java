e9e4fe2323e80cf485615c8896c3fd49b8da9135
egor-bryzgalov <63807308+egor-bryzgalov@users.noreply.github.com>
2021-06-26 13:42:40 +0300

    Added WebView app

---
+++ app/src/main/java/com/example/gservice_view/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.gservice_view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+     private final String Url = "https://google.com";
+     private WebViewClient webViewClient = new WebViewClient() {
+
+         @SuppressWarnings("deprecation")
+         @Override
+         public boolean shouldOverrideUrlLoading(WebView view, String url) {
+             view.loadUrl(url);
+             return true;
+         }
+
+         @TargetApi(Build.VERSION_CODES.N)
+         @Override
+         public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+             view.loadUrl(request.getUrl().toString());
+             return true;
+         }
+     };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webView);
+
+        webView.loadUrl(this.Url);
+        webView.setWebViewClient(this.webViewClient);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        this.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+}
