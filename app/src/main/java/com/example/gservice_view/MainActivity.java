0c2f5b3de607405606edae151db808303ba71ebf
egor-bryzgalov <63807308+egor-bryzgalov@users.noreply.github.com>
2021-06-26 14:00:24 +0300

    Changed back button handler

--- app/src/main/java/com/example/gservice_view/MainActivity.java
+++ app/src/main/java/com/example/gservice_view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gservice_view;
 
+import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.TargetApi;
@@ -39,6 +40,16 @@ public class MainActivity extends AppCompatActivity {
         webView.loadUrl(this.Url);
         webView.setWebViewClient(this.webViewClient);
         webView.getSettings().setJavaScriptEnabled(true);
+
+        OnBackPressedCallback callback = new OnBackPressedCallback(true /* enabled by default */) {
+            @Override
+            public void handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack();
+                }
+            }
+        };
+        this.getOnBackPressedDispatcher().addCallback(this, callback);
 
         this.getWindow().getDecorView().setSystemUiVisibility(
                 View.SYSTEM_UI_FLAG_LAYOUT_STABLE
